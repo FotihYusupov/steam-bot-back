e2ba5b152a8088e39a2c04d817773344fbffbb1f
FotihYusupov <yusupovfotih158@gmail.com>
2025-06-29 21:07:18 +0500

    fix: get skinsPort prices issue

--- src/integrations/skinsPort.js
+++ src/integrations/skinsPort.js
@@ -1,11 +1,19 @@
-const cron = require("node-cron");
 const axios = require('axios');
+const cron = require('node-cron');
 const Item = require("../models/Item");
 
 async function getSkinsportPrice() {
   try {
     const url = `https://api.skinport.com/v1/items?currency=USD`;
-    const { data } = await axios.get(url);
+
+    const headers = {
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36',      
+      'Accept': 'application/json',
+      'Cache-Control': 'no-cache',
+      'Pragma': 'no-cache',
+    };
+
+    const { data } = await axios.get(url, { headers });
 
     for (let i = 0; i < data.length; i++) {
       const item = data[i];
@@ -36,11 +44,20 @@ async function getSkinsportPrice() {
     }
   } catch (err) {
     console.error("Error fetching or saving item prices:", err);
+    if (err.response) {
+      console.error("Status:", err.response.status);
+      console.error("Data:", err.response.data);
+      console.error("Headers:", err.response.headers);
+    } else if (err.request) {
+      console.error("No response received:", err.request);
+    } else {
+      console.error("Error setting up request:", err.message);
+    }
   }
 }
 
 cron.schedule(
-  "0 */4 * * *",
+  "0 */2 * * *",
   () => {
     console.log("Starting get skinsport prices...");
     getSkinsportPrice();
